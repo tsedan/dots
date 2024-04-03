3dd1c4d63887c152e4e463e6a12eb1ea1e3a2c6e
Tomer Sedan <tsedan@proton.me>
2024-04-03 17:03:35 -0400

    neovim config

---
+++ .config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,12 @@
+return {
+  -- add moonbow
+  { "arturgoms/moonbow.nvim" },
+
+  -- Configure LazyVim to load moonbow
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "moonbow",
+    },
+  }
+}
