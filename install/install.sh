5c6898248f91b8e47c8a79b246b0079a735f840b
Tomer Sedan <tsedan@proton.me>
2024-07-28 12:15:18 -0700

    set shell in kitty

--- install/install.sh
+++ install/install.sh
@@ -4,29 +4,21 @@ fprint() {
   echo "\e[34m$1\e[0m"
 }
 
-get_install_tools() {
-  PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
-  if [[ "$PLATFORM" == 'darwin' ]]; then
-    # install homebrew if not already installed
-    if ! command -v brew &>/dev/null; then
-      fprint "Installing brew"
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
-
-    # set package install command
-    INSTALL_CMD="brew install"
-  else
-    # set package install command
-    INSTALL_CMD="sudo apt install -y"
+PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+if [[ "$PLATFORM" == 'darwin' ]]; then
+  if ! command -v brew &>/dev/null; then
+    fprint "Installing brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
-}
+
+  INSTALL_CMD="brew install"
+else
+  INSTALL_CMD="sudo apt install -y"
+fi
 
 get_install_tools
 fprint "Installing packages"
 $INSTALL_CMD "$(curl -fsSL https://raw.githubusercontent.com/tsedan/dots/main/install/reqs.txt)"
-
-fprint "Switching shell to zsh"
-chsh -s $(which zsh)
 
 fprint "Cloning dotfiles repo"
 git clone https://github.com/tsedan/dots.git ~/dots
