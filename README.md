4f3f5e0095c80dc19cdf942320bc62851df19883
Tomer Sedan <tsedan@proton.me>
2024-07-28 12:04:57 -0700

    add brew install script

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ They are meant to be as minimal as possible.
 
 They have the particular requirement that all packages must be available for both Mac (Homebrew) and Ubuntu (apt). I also sometimes test them on Fedora.
 
+To install:
+```shell
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/tsedan/dots/main/install/install.sh)"
+```
+
--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+fprint() {
+  echo "\e[34m$1\e[0m"
+}
+
 get_install_tools() {
   PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
   if [[ "$PLATFORM" == 'darwin' ]]; then
     # install homebrew if not already installed
+    if ! command -v brew &>/dev/null; then
+      fprint "Installing brew"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
 
     # set package install command
     INSTALL_CMD="brew install"
@@ -14,10 +22,15 @@ get_install_tools() {
 }
 
 get_install_tools
-PACKAGES=$(curl -s "https://raw.githubusercontent.com/tsedan/dots/main/install/reqs.txt")
-$INSTALL_CMD $PACKAGES
+fprint "Installing packages"
+$INSTALL_CMD "$(curl -fsSL https://raw.githubusercontent.com/tsedan/dots/main/install/reqs.txt)"
 
+fprint "Switching shell to zsh"
 chsh -s $(which zsh)
+
+fprint "Cloning dotfiles repo"
 git clone https://github.com/tsedan/dots.git ~/dots
+
+fprint "Stowing dotfiles"
 cd ~/dots
 stow .
