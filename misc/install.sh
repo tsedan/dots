584755eb3ddbdc29287d783e4b0bc3240b78a7e9
Tomer Sedan <tsedan@proton.me>
2024-07-29 20:56:07 -0700

    fix installation order

--- misc/install.sh
+++ misc/install.sh
@@ -8,72 +8,63 @@ eprint() {
   printf "\033[31m$1\033[0m\n"
 }
 
-detect_os() {
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if [ -f /etc/os-release ]; then
-      . /etc/os-release
-      OS=$NAME
-    elif [ -f /etc/lsb-release ]; then
-      . /etc/lsb-release
-      OS=$DISTRIB_ID
-    else
-      OS=$(uname -s)
-    fi
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    OS="MacOS"
-  else
-    OS="unknown"
-  fi
-}
-
-get_package_tools() {
-  if [[ "$OS" == "Ubuntu" || "$OS" == "Debian GNU/Linux" ]]; then
-    cprint "Updating packages"
-    sudo apt update -qq && sudo apt upgrade -qq
-    INSTALL_CMD="sudo apt install -qqq -y"
-  elif [[ "$OS" == "MacOS" ]]; then
-    if ! type brew &>/dev/null; then
-      cprint "Installing homebrew"
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
-
-    cprint "Updating packages"
-    brew update -q && brew upgrade -q
-    INSTALL_CMD="brew install -q"
-  else
-    eprint "Unsupported OS: $OS"
-    exit 1
-  fi
-}
-
 cprint "Authenticating"
 sudo -v
 
-detect_os
-get_package_tools
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$NAME
+  elif [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+  else
+    OS=$(uname -s)
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  OS="MacOS"
+else
+  OS="unknown"
+fi
 
-if [[ -z $UPDATE ]]; then
-  cprint "Setting default shell"
-  sudo sh -c "echo $(which zsh) >> /etc/shells"
-  chsh -s $(which zsh)
+if [[ "$OS" == "Ubuntu" || "$OS" == "Debian GNU/Linux" ]]; then
+  cprint "Updating packages"
+  sudo apt update -qq && sudo apt upgrade -qq
+  INSTALL_CMD="sudo apt install -qqq -y"
+elif [[ "$OS" == "MacOS" ]]; then
+  if ! type brew &>/dev/null; then
+    cprint "Installing homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  cprint "Updating packages"
+  brew update -q && brew upgrade -q
+  INSTALL_CMD="brew install -q"
+else
+  eprint "Unsupported OS: $OS"
+  exit 1
 fi
 
 if [[ ! -d ~/dots ]]; then
+  cprint "Installing dependencies"
+  PACKAGES=$(curl -fsSL https://raw.githubusercontent.com/tsedan/dots/main/misc/reqs.txt)
+  $INSTALL_CMD $PACKAGES
+
   cprint "Cloning dotfiles repo"
   git clone --recurse-submodules https://github.com/tsedan/dots.git ~/dots
+
+  cprint "Setting default shell"
+  sudo sh -c "echo $(which zsh) >> /etc/shells"
+  chsh -s $(which zsh)
+else
+  cprint "Updating dotfiles"
+  cd ~/dots
+  stow -D .
+  git pull
+  git submodule update --init --recursive
 fi
 
-cprint "Updating dotfiles"
-cd ~/dots
-stow -D .
-git pull
-git submodule update --init --recursive
 stow .
 
 cprint "Validating dependencies"
-if [[ -f ~/dots/misc/reqs.txt ]]; then
-  PACKAGES=$(cat ~/dots/misc/reqs.txt)
-else
-  PACKAGES=$(curl -fsSL https://raw.githubusercontent.com/tsedan/dots/main/misc/reqs.txt)
-fi
+PACKAGES=$(cat ~/dots/misc/reqs.txt)
 $INSTALL_CMD $PACKAGES
