04031e8c9e490e379c76053de93c9cd3a6d57a79
Tomer Sedan <tsedan@proton.me>
2024-08-29 11:26:05 -0400

    fish prompt improvement

--- stow/.config/fish/functions/fish_prompt.fish
+++ stow/.config/fish/functions/fish_prompt.fish
@@ -1,51 +1,16 @@
 function fish_prompt
-  set -l last_pipestatus $pipestatus
-  set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-  set -l normal (set_color normal)
-  set -q fish_color_status
-  or set -g fish_color_status red
+  set -l color_host $fish_color_host
+  if set -q SSH_CLIENT
+    set color_host $fish_color_host_remote
+  end
 
-  # Color the prompt differently when we're root
   set -l color_cwd $fish_color_cwd
-  set -l suffix '>'
-  if functions -q fish_is_root_user; and fish_is_root_user
-      if set -q fish_color_cwd_root
-          set color_cwd $fish_color_cwd_root
-      end
-      set suffix '#'
+  if fish_is_root_user
+    set color_cwd $fish_color_cwd_root
   end
 
-  # Write pipestatus
-  # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
-  set -l bold_flag --bold
-  set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
-  if test $__fish_prompt_status_generation = $status_generation
-      set bold_flag
-  end
-  set __fish_prompt_status_generation $status_generation
-  set -l status_color (set_color $fish_color_status)
-  set -l statusb_color (set_color $bold_flag $fish_color_status)
-  set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-
-  echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
-
-  # set -l color_host $fish_color_host
-  # if set -q SSH_CLIENT
-  #   set color_host $fish_color_host_remote
-  # end
-
-  # set -l color_cwd $fish_color_cwd
-  # set -l prefix
-  # set -l suffix '%'
-  # if contains -- $USER root toor
-  #   if set -q fish_color_cwd_root
-  #     set color_cwd $fish_color_cwd_root
-  #   end
-  #   set suffix '#'
-  # end
-
-  # printf '%s%s%s @ %s%s%s %s ' \
-  #   (set_color $color_host) $USER (set_color brblack) \
-  #   (set_color $color_cwd) (prompt_pwd) (set_color brblack) \
-  #   $suffix
+  printf '%s%s%s @ %s%s%s %s ' \
+    (set_color $color_host) $USER (set_color brblack) \
+    (set_color $color_cwd) (prompt_pwd) (set_color brblack) \
+    $prompt_suffix
 end
